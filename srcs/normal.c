cfb76a6303016e4869af53a5832ad89e571d9e60
YassineMekraoui <yassinemekraoui@gmail.com>
2019-04-13 10:21:32 +0200

    srcs/normal.c mis a la norme

--- srcs/normal.c
+++ srcs/normal.c
@@ -6,13 +6,13 @@
 /*   By: ymekraou <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/03/10 18:48:02 by ymekraou          #+#    #+#             */
-/*   Updated: 2019/04/03 08:08:23 by ymekraou         ###   ########.fr       */
+/*   Updated: 2019/04/13 10:20:50 by ymekraou         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../includes/rtv1.h"
 
-void 	normal_sphere(double sphere_center[3], t_hit *hit_point)
+void	normal_sphere(double sphere_center[3], t_hit *hit_point)
 {
 	int i;
 
@@ -22,7 +22,7 @@ void 	normal_sphere(double sphere_center[3], t_hit *hit_point)
 	norm_vector(hit_point->normal);
 }
 
-void 	normal_plan(double normal_plan[3], t_hit *hit_point)
+void	normal_plan(double normal_plan[3], t_hit *hit_point)
 {
 	int i;
 
@@ -34,15 +34,16 @@ void 	normal_plan(double normal_plan[3], t_hit *hit_point)
 
 void	normal_cylender(t_cylender *cylender, t_hit *hit_point)
 {
-	double tmp[3];
-	double t;
-	int i;
+	double	tmp[3];
+	double	t;
+	int		i;
 
-	t =  -(dot_product(cylender->origin_relative, cylender->line_vector_relative)
+	t = -(dot_product(cylender->origin_relative, cylender->line_vector_relative)
 		- dot_product(hit_point->coord, cylender->line_vector_relative));
 	i = -1;
 	while (++i < 3)
-		tmp[i] = cylender->origin_relative[i] + t * cylender->line_vector_relative[i];
+		tmp[i] = cylender->origin_relative[i]
+			+ t * cylender->line_vector_relative[i];
 	i = -1;
 	while (++i < 3)
 		hit_point->normal[i] = (hit_point->coord[i] - tmp[i]);
@@ -51,9 +52,9 @@ void	normal_cylender(t_cylender *cylender, t_hit *hit_point)
 
 void	normal_cone(t_cone *cone, t_hit *hit_point)
 {
-	double tmp[3];
-	double t;
-	int i;
+	double	tmp[3];
+	double	t;
+	int		i;
 
 	i = -1;
 	while (++i < 3)
