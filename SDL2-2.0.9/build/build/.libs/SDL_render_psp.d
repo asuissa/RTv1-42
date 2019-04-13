999afb9d187a604b1be6c95b074c5fda84e77e7a
YassineMekraoui <yassinemekraoui@gmail.com>
2019-04-13 09:10:21 +0200

    suppression du dossier SDL2-2.0.9

--- SDL2-2.0.9/build/build/.libs/SDL_render_psp.d
+++
@@ -1,9 +0,0 @@
-build/SDL_render_psp.lo: \
-  /Users/asuissa/Documents/rtv1/SDL2-2.0.9/src/render/psp/SDL_render_psp.c \
-  /Users/asuissa/Documents/rtv1/SDL2-2.0.9/src/render/psp/../../SDL_internal.h \
-  /Users/asuissa/Documents/rtv1/SDL2-2.0.9/src/render/psp/../../dynapi/SDL_dynapi.h \
-  /Users/asuissa/Documents/rtv1/SDL2-2.0.9/src/render/psp/../../dynapi/SDL_dynapi_overrides.h \
-  include/SDL_config.h \
-  /Users/asuissa/Documents/rtv1/SDL2-2.0.9/include/SDL_platform.h \
-  /Users/asuissa/Documents/rtv1/SDL2-2.0.9/include/begin_code.h \
-  /Users/asuissa/Documents/rtv1/SDL2-2.0.9/include/close_code.h
